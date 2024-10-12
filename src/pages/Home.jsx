ab34eab1ba05af5b70e997d56f52ef4e0c55b1fd
doki <cov6155@gmail.com>
2024-10-12 18:46:43 +0700

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
   useEffect(() => {
     const url =
       process.env.NODE_ENV === "production"
-        ? "https://video-confe-client.onrender.com"
+        ? "https://back-real-time-video-group-back.onrender.com"
         : "http://localhost:5000";
 
     console.log(`Environment: ${process.env.NODE_ENV}`);
